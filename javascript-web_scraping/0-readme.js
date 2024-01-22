c6607d7185a7ac1de863d9944d70d6e4bbac9756
Aicha-ch <chwikhi.aycha@gmail.com>
2024-01-22 15:32:38 +0100

    update

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', function (err, data) {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
-    console.log(data);
+      console.log(data);
   }
 });
