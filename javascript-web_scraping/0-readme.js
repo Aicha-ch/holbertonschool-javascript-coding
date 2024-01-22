8fce5b63cb7cfa065dc6c76fc21cd2167bf77924
Aicha-ch <chwikhi.aycha@gmail.com>
2024-01-22 15:29:42 +0100

    update code

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: node 0-readme.js <file-path>');
-  process.exit(1);
-}
-
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     console.log(data);
   }
