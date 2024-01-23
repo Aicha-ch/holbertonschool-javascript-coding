056b6b0e44ac7a7fca2246567a44009e75f29d2c
Aicha-ch <chwikhi.aycha@gmail.com>
2024-01-23 15:41:56 +0100

    update

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,22 @@
 #!/usr/bin/node
-
-// Write a script that computes the number of tasks completed by user id.
-
 const request = require('request');
 
-const url = process.argv[2];
+request(process.argv[2], (error, request, body) => {
+  if (error) {
+    console.log(error);
+  }
 
-request.get(url, function (err, response, body) {
-  if (err) {
-    console.log('error:', err);
-  } else {
-    const obj = JSON.parse(body);
-    const tasks = {};
-    for (const task of obj) {
-      if (task.completed === true) {
-        if (tasks[task.userId] === undefined) {
-          tasks[task.userId] = 1;
-        } else {
-          tasks[task.userId] += 1;
-        }
+  const obj = JSON.parse(body);
+  const completedTasks = {};
+  for (const task of obj) {
+    if (task.completed) {
+      if (task.userId in completedTasks) {
+        completedTasks[task.userId] += 1;
+      } else {
+        completedTasks[task.userId] = 1;
       }
     }
-    console.log(tasks);
   }
+
+  console.log(completedTasks);
 });
